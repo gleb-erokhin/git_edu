7d6faa0522476432ede946ba54049e4992fcbd41
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-11 01:01:37 +0300

    test ya file

--- test.py
+++ test.py
@@ -1,2 +1,37 @@
-print("hello word!")
-print
+#name = input("Как вас зовут?")
+#print(f"Привет, {name}")
+
+#name = input("Как Вас зовут? ")
+#print("Привет,", name)
+
+#print("2 + 2 = 4" * 3, sep="\n")
+
+#phrase = input()
+#print(phrase * 3)
+
+#name = input("Как Вас зовут? ")
+#print('Привет, %' .format(name))
+
+#name = input("Как Вас зовут? ")
+#print("Привет, ", name, sep="")
+
+# goods = input()
+# price = int(input())
+# weight = int(input())
+# cash = int(input())
+# sum = cash - (weight * price)
+# print('Чек')
+# print(f'{goods} - {weight}кг - {price}руб/кг')
+# print(f"Итого: {weight * price}руб")
+# print(f"Внесено: {cash}руб")
+# print(f"Сдача: {sum}руб")
+
+#username = input("Как Вас зовут? ")
+#print('Привет, %s' % username)
+
+# phrase = int(input())
+# print(phrase * "Купи слона!\n")
+
+count = int(input())
+phrase = input()
+print(f'Я больше никогда не буду писать "{phrase}"!\n' * count)
