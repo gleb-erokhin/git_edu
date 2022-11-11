0ff2aefa3686df1b1f817e2e78a4b6f25641f454
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-11 08:06:54 +0300

    second day ya task

--- test.py
+++ test.py
@@ -32,6 +32,43 @@
 # phrase = int(input())
 # print(phrase * "Купи слона!\n")
 
-count = int(input())
-phrase = input()
-print(f'Я больше никогда не буду писать "{phrase}"!\n' * count)
+# count = int(input())
+# phrase = input()
+# print(f'Я больше никогда не буду писать "{phrase}"!\n' * count)
+
+# minute = int(input())
+# children = int(input())
+# sum = (minute / 2) * children
+# print(int(sum))
+
+# name_child = input()
+# number = int(input())
+# group = number // 100
+# print(f"Группа №{group}.")
+# print(f"{number % 10}. {name_child}.")
+# print(f"Шкафчик: {number}.")
+# print(f"Кроватка: {number // 10 % 10}.")
+
+# number = int(input())
+# first = number % 100
+# first_1 = first % 10
+# first_2 = first // 10
+# second = number // 100
+# second_1 = second % 10
+# second_2 = second // 10
+# print(f"{second_1}{second_2}{first_1}{first_2}")
+
+num_1 = int(input())
+num_2 = int(input())
+num_1_1 = num_1 % 10
+num_1_2 = num_1 // 10 % 10
+num_1_3 = num_1 // 100
+num_2_1 = num_2 % 10
+num_2_2 = num_2 // 10 % 10
+num_2_3 = num_2 // 100
+sum1 = (num_1_1 + num_2_1) % 10
+sum2 = (num_1_2 + num_2_2) % 10
+sum3 = (num_1_3 + num_2_3) % 10
+print(num_1_3, num_1_2, num_1_1)
+print(num_2_3, num_2_2, num_2_1)
+print(f"{sum3}{sum2}{sum1}")
